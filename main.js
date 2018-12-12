44cdaedcc12c1f9697d7b159aade5edc6e940aac
ip-d <singhjaskaran2002>
2018-12-12 18:34:55 +0530

    get user

--- main.js
+++ main.js
@@ -7,22 +7,9 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json(), cors());
 
-app.get('/listUsers', function (req, res) {
-	fs.readFile(__dirname + '/' + "users.json", function (err, data) {
-		console.log(data);
-		console.log("api called");
-		res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-		// res.header("Access-Control-Allow-Headers");RegisterComponent
-		res.writeHeader(200, { 'Content-type': 'application/json' });
-		res.end(data);
-	});
-});
-
 app.post('/login', function (req, res) {
 	fs.readFile(__dirname + '/' + "users.json", function (err, data) {
-		console.log('request body: ', req.body);
 		res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-		// res.header("Access-Control-Allow-Headers", {'Content-type':'application/json'});
 		var info = JSON.parse(data);
 		var index = -1;
 
@@ -41,9 +28,59 @@ app.post('/login', function (req, res) {
 			res.write(JSON.stringify({ status: 'false', message: 'Invalid email and password' }));
 			res.end();
 		} else {
-			res.writeHeader(200, { 'Content-type': 'application/json' });
-			res.write(JSON.stringify({ status: 'true', message: 'Logged in' }));
+			res.writeHeader(200, { 'Content-type': 'application/json' }); false
+			res.write(JSON.stringify({ status: 'true', message: 'Logged in', accessToken: '2002' }));
 			res.end();
+		}
+	});
+});
+
+app.post('/register', function (req, res) {
+	fs.readFile(__dirname + '/' + "users.json", function (err, data) {
+		res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+		var info = JSON.parse(data);
+		var flag = true;
+
+		for (let i = 0; i < info.users.length; i++) {
+			if (req.body.email === info.users[i].email) {
+				flag = false;
+			}
+		}
+
+		if (flag) {
+			info.users.push(req.body);
+		}
+
+		fs.writeFile(__dirname + '/' + "users.json", JSON.stringify(info), function (err) {
+			if (err) {
+				console.log(err.stack());
+				res.writeHeader(200, { 'Content-type': 'application/json' });
+				res.write(JSON.stringify({ status: 'false', message: 'record not added' }));
+				res.end();
+			} else if (!flag) {
+				res.writeHeader(200, { 'Content-type': 'application/json' });
+				res.write(JSON.stringify({ status: 'taken', message: 'Email already taken' }));
+				res.end();
+			} else {
+				res.writeHeader(200, { 'Content-type': 'application/json' });
+				res.write(JSON.stringify({ status: 'true', message: 'record added' }));
+				res.end();
+			}
+		});
+	});
+});
+
+app.get('/get', function (req, res) {
+	fs.readFile(__dirname + '/' + "users.json", function (err, data) {
+		var info = JSON.parse(data);
+		if (err) {
+			res.writeHeader(200, { 'Content-type': 'application/json' });
+			res.write(JSON.stringify({ status: 'false', message: 'File not found' }));
+			res.end();
+		} else {
+			res.writeHeader(200, { 'Content-type': 'application/json' });
+			res.write(JSON.stringify({ status: 'true', message: 'records sent' }));
+			res.end(info);
 		}
 	});
 });
